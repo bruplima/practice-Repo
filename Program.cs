1f98c833cd020bd259db87d3f8f174dde63e8576
bruplima <149356556+bruplima@users.noreply.github.com>
2023-11-14 10:35:00 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿
+//Array- is a structure, and is uniform, and consistent
+
+//float[] temperatures;
+//new float[20];
+//float [] temperatures ={ 0.5f,1,2,-1.5f,3,2,0};
+
+//float []temperatures = new float[] { 0.5f, 1, 2, -1.5f, 3, 2, 0 };
+
+
+float[] temperatures = new float [7] { 0.5f, 1, 2, -1.5f, 3, 2, 0 };
+
+foreach (float t in temperatures)
+{
+    Console.WriteLine(t);
+} ;
+
+//Console.WriteLine(temperatures);
+
+//float[] temperatutes = new float[20];
+
+
+//Console.WriteLine($"{temperatutes}");
+
+
+
+
+
